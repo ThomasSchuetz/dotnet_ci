dfd04cf16896b7dc8761633a12ded09de369d8fd
Thomas Schuetz <thomas.schuetz@eon.com>
2021-09-11 11:44:46 +0200

    Added calculator and (working) test code

---
+++ src/Calculator/Calculator.Tests/SimpleCalculatorTests.cs
@@ -0,0 +1,46 @@
+using System;
+using Xunit;
+
+namespace Calculator.Tests
+{
+    public class SimpleCalculatorTests
+    {
+        private readonly SimpleCalculator calculator;
+        public SimpleCalculatorTests() => calculator = new SimpleCalculator();
+
+        [Fact]
+        public void Adding_positive_number_increases_value()
+        {
+            var anyPositiveNumber = 13.2;
+
+            calculator.Add(anyPositiveNumber);
+
+            Assert.Equal(anyPositiveNumber, calculator.Value);
+        }
+
+        [Fact]
+        public void Subtracting_negative_number_increases_value()
+        {
+            var anyNegativeNumber = -2;
+
+            calculator.Subtract(anyNegativeNumber);
+
+            Assert.Equal(-anyNegativeNumber, calculator.Value);
+        }
+
+        [Fact]
+        public void Division_by_zero_throws()
+        {
+            Assert.Throws<DivideByZeroException>(() => calculator.Divide(0));
+        }
+
+        [Fact]
+        public void Division_by_notZero_does_not_throw()
+        {
+            var anyNonZeroNumber = 3.17;
+
+            var exception = Record.Exception(() => calculator.Divide(anyNonZeroNumber));
+            Assert.Null(exception);
+        }
+    }
+}
---
+++ src/Calculator/Calculator/SimpleCalculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class SimpleCalculator
+    {
+        public double Value { get; private set; }
+
+        public void Add(double number) => this.Value += number;
+
+        public void Subtract(double number) => this.Value -= number;
+
+        public void Multiply(double number) => this.Value *= number;
+
+        public void Divide(double number)
+        {
+            if (number == 0)
+            {
+                throw new DivideByZeroException("Cannot divide by 0");
+            }
+
+            this.Value /= number;
+        }
+    }
+}
